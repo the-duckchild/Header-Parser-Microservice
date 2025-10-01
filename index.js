07ed0296f26329bf334363ffa6dfa95c02025468
Simon Duck <its.duck@gmail.com>
2025-10-01 15:30:44 +0100

    Header parser complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -21,7 +22,7 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  res.json({ greeting: 'hello API' });
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
 // listen for requests :)
